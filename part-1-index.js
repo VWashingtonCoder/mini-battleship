c64aef02fef90bc23b46937f34f8da58391f5e9f
V. Andre Washington <kuraiokamiblaze@gmail.com>
2022-12-16 13:43:45 -0700

    Fulfill requirements 1&2
    Updated for DRY method

--- part-1-index.js
+++ part-1-index.js
@@ -1,28 +1,29 @@
 var rs = require('readline-sync');
 
-let boardArr = [
+let board = [
     'A1', 'A2', 'A3', 
     'B1', 'B2', 'B3', 
     'C1', 'C2', 'C3'
 ];
-
-let shipOne = undefined;
-let shipTwo = undefined;
+let ships = [];
 
 function randomNumber(max) { return Math.floor(Math.random() * max); }
 
-function setShip(board) {
-    let randomShipId = randomNumber(board.length);
-    let shipCode = boardArr[randomShipId];
-    return shipCode;
+function setShips() {
+    const shipOne = board[randomNumber(board.length)];
+    const removedBoard = board.filter((ship) => ship !== shipOne);
+    const shipTwo = removedBoard[randomNumber(removedBoard.length)];
+
+    ships.push(shipOne, shipTwo);
 }
+
+
 
 const start = rs.keyIn('Press any key to start the game');
 
 if (start) {
-    shipOne = setShip(boardArr);
-    const removedBoard = boardArr.filter((ship) => ship !== shipOne);
-    shipTwo = setShip(removedBoard);
+    setShips();
 };
 
+console.log(ships);
 
